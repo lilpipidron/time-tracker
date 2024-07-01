070a2ae7f7b4df4e5d27644cc73bebc1a253baa9
vadim <lociX55Lim@gmail.com>
2024-07-01 13:19:46 +0300

    feat: add migrations (go)

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/lilpipidron/time-tracker
+
+go 1.22.4
+
+require (
+	github.com/caarlos0/env/v11 v11.1.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.17.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sethvargo/go-envconfig v1.0.3 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
---
+++ internal/storage/postgresql/postgresql.go
@@ -0,0 +1,40 @@
+package postgresql
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+)
+
+type Storage struct {
+	DB *sql.DB
+}
+
+func NewPostgresDB(psqlInfo, dbname string) (*Storage, error) {
+	const errFunc = "storage.postgresql.NewStorage"
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", errFunc, err)
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", errFunc, err)
+	}
+
+	migration, err := migrate.NewWithDatabaseInstance("file://internal/storage/postgresql/migrations", dbname, driver)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", errFunc, err)
+	}
+
+	if err := migration.Up(); err != nil {
+		if !errors.Is(err, migrate.ErrNoChange) {
+			return nil, fmt.Errorf("%s: %w", errFunc, err)
+		}
+	}
+
+	return &Storage{DB: db}, nil
+}
